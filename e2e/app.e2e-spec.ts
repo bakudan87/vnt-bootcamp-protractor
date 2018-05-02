656f8b4d5357f9fd16d70e1ad3660371856d033f
vntrool <rogerio.oliveira@venturus.org.br>
2018-05-02 10:58:26 -0300

    Add support to read data from json file
    Add helper to read files

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,29 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('bootcamp-protractor App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(element(by.id("welcomeMsg")).isDisplayed()).toBe(true);
   });
+
+  all(require('../testData/loginInfo.json'), (data, iteration) => {
+    fit(`iteration #${iteration}: login attempt for ${data.description}`, () => {
+      element(by.id('inputUsername')).sendKeys(data.username);
+      element(by.id('inputPassword')).sendKeys(data.password);
+      element(by.id('loginSubmit')).click();
+      
+      expect(element(by.id("login-alert-message")).getText()).toBe(data.output);
+      //expect(element(by.id("login-alert-message")).isDisplayed()).toBe(true);
+      
+      element(by.id('inputUsername')).clear();
+      element(by.id('inputPassword')).clear();
+      })
+})
 });
